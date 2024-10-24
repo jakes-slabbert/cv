1acddb266d859a6680aaeea36c2b9d8c399738d4
jakes-slabbert <jako.slabbert@gmail.com>
2024-10-24 16:43:36 +0200

    Final for Texas

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,24 @@
 /*!
 * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const images = [
+    'assets/coding_1.png',
+    'assets/kidlike.png',
+    'assets/ninja.png',
+    'assets/profile_ash.png',
+    'assets/profile.png'
+];
+
+let currentIndex = 0;
+const profileImg = document.getElementById('profile-img');
+
+// Function to update the image
+function updateImage() {
+    profileImg.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image
+}
+
+// Set an interval to change the image every 30 seconds (30000ms)
+setInterval(updateImage, 30000);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,19 @@
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const images = [
+    'assets/coding_1.png',
+    'assets/kidlike.png',
+    'assets/ninja.png',
+    'assets/profile_ash.png',
+    'assets/profile.png'
+];
+
+let currentIndex = 0;
+const profileImg = document.getElementById('profile-img');
+
+// Function to update the image
+function updateImage() {
+    profileImg.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image
+}
+
+// Set an interval to change the image every 30 seconds (30000ms)
+setInterval(updateImage, 30000);
